cba72d853d10f0ca1bd04953f09a82b2d9d37941
Josde <contact@josde.me>
2023-10-16 19:35:51 +0200

    Make object have everything be optional except id and type, according to spec

--- schemas/object.py
+++ schemas/object.py
@@ -10,32 +10,31 @@ Collection = ForwardRef('Collection')
 class Object(BaseModel):
     id : str 
     type : str
-    name : str
+    name : Optional(str)
     attachment : Optional(str)
     attributedTo : Optional(Object)
     audience : Optional([Object])
-    content : str = "https://www.w3.org/ns/activitystreams"
+    content : Optional(str) = "https://www.w3.org/ns/activitystreams"
     bcc : Optional([Union[Object, Link]])
     bto : Optional([Union[Object, Link]]) 
     cc : Optional([Union[Object, Link]])
     context : Optional(Union[Object, Link])
-    name : str
-    endTime : datetime
+    endTime : Optional(datetime)
     generator : Optional(Union[Object, Link])
     icon : Optional(Union[Image, Link])
     image : Optional(Union[Image, Link])
     inReplyTo : Optional(Union[Object, Link])
     location : Optional(Union[Object, Link])
     preview : Optional(Union[Object, Link])
-    published : datetime
+    published : Optional(datetime)
     replies : Optional(Collection)
-    startTime : datetime
-    summary : str
+    startTime : Optional(datetime)
+    summary : Optional(str)
     tag : Optional(Union[Object, Link])
-    updated : datetime
+    updated : Optional(datetime)
     url : Optional(Union[str, Link])
     to : Optional([Union[Object, Link]])
     mediaType : Optional(Link)
-    duration : timedelta # should be duration
+    duration : Optional(timedelta) # TODO: should be a duration-equiuvalent class for proper serialization
 
 Object.model_rebuild()
